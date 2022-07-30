d0b76d657bb8fb21006a0d6e980070ebafd16493
JakaV2 <106079586+JakaV2@users.noreply.github.com>
2022-07-31 06:57:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.pulsa = ''
 global.gopay = ''
 
 //=============『 Info Bot 』=================//
-global.namebot = 'Xziro'
+global.namebot = 'Self Bot'
 global.gc = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
 global.web = 'hp' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
@@ -23,10 +23,10 @@ global.fotonya1 = 'https://database.tioclkp02.repl.co/TextPro.me_162df9a5371cb2.
 global.fotonya2 = 'https://database.tioclkp02.repl.co/TextPro.me_162df9a5371cb2.jpg' //ini juga ganti 
 global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '© xziro'
+global.wm = '© Self Bot'
 global.watermark = wm
-global.wm2 = '⫹⫺ xziro'
-global.wm3 = '© xziro'
+global.wm2 = '⫹⫺ Self Bot'
+global.wm3 = '© Self Bot'
 global.wm4 = 'Ⓛ = Limit Ⓟ = Premium'
 global.htki = '––––––『' 
 global.htka = '』––––––'
@@ -38,7 +38,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made with'
-global.author = 'Bot WhatsApp'
+global.author = 'Self Bot'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
